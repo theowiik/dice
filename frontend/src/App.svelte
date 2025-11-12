58b00da2eda41a620656a4eb5b0746848624546f
theowiik <theo.le.wiik@gmail.com>
2025-11-12 20:25:41 +0100

    Remove boilerplate

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,12 +1,1 @@
-<main>
-  <h1>Hello world</h1>
-</main>
-
-<style>
-  main {
-    min-height: 100vh;
-    display: grid;
-    place-items: center;
-    font-family: system-ui, sans-serif;
-  }
-</style>
+<main></main>
